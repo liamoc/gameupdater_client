2adb15f84ed840191d24b0bf918a1d497c64f956
dom <dom@a378cb3c-2f42-0410-bc76-82916d725bbd>
2007-12-26 14:18:45 +0000

    lots more functionality

--- GameUpdater.js
+++ GameUpdater.js
@@ -10,7 +10,7 @@ function GameUpdater(url)
 	// Where to look for version information.
 	// This can be safely changed as long as it is always consistent.
 	this.versionFile = "version";
-	
+		
 	// Progress callback. Hook into this to receive feedback from the updater.
 	// (int progress, string working_on) => void
 	this.onProgress = null;	
@@ -35,7 +35,7 @@ function GameUpdater(url)
 		
 		for (var i = 0; i < localVersionData.length; i++)
 		{
-			localVersionInfo[localVersionData[i].substr(0, localVersionData[i].indexOf("="))] = localVersionData[i].substr(localVersionData[i].indexOf("="));
+			localVersionInfo[localVersionData[i].substr(0, localVersionData[i].indexOf("="))] = localVersionData[i].substr(localVersionData[i].indexOf("=") + 1);
 		}
 	
 		if (!localVersionInfo['version'] || !localVersionInfo['dispVersion'])
@@ -64,7 +64,6 @@ function GameUpdater(url)
 	
 		// Find out what the available version is from the server.
 		remoteVersionInfo = getRemoteVersionInfo();
-	
 		if (remoteVersionInfo['version'] > localVersionInfo['version'])
 		{
 			if (this.onVersionAvailable && this.onVersionAvailable(remoteVersionInfo))
@@ -84,25 +83,82 @@ function GameUpdater(url)
 				}
 			}
 		}
+		else
+		{
+			return true;
+		}
+	}
+	
+	function fileExists(file)
+	{
+		// TODO: implement this
+		return false;
 	}
 	
 	function performUpdate()
 	{
+		// Get the hashes, so we know what files need updating.
+		var hashes = getHashes();
+		
+		// Check all our local files
+		var requires_update = [];
+		for (var file in hashes)
+		{
+			if (fileExists(file))
+			{
+				var localHash = HashFromFile("../" + file);
+				if (hashes[file] != localHash)
+					requires_update.push(file);
+			}
+			else
+			{
+				requires_update.push(file);
+			}
+		}
+		
+	}
 	
+	function getHashes()
+	{
+		try
+		{
+			var request = new HttpRequest(url + "/get/hashes", "GET");
+			request.send();
+			var hashesData = CreateStringFromByteArray(request.readAll()).split(/\r?\n/);
+			request.close();
+		}
+		catch (e)
+		{
+			throw new GameUpdaterException("Unable to request hashes resource.\n");
+		}
+
+		var hashesInfo = {};
+		for (var i = 0; i < hashesData.length; i++)
+		{
+			hashesInfo[hashesData[i].substr(0, hashesData[i].lastIndexOf(" "))] = hashesData[i].substr(hashesData[i].lastIndexOf(" ") + 1);
+		}
+		return hashesInfo;
 	}
 	
 	function getRemoteVersionInfo()
 	{
-		var request = new HttpRequest(url + "/get/version", "GET");
-		request.send();
-		var remoteVersionData = CreateStringFromByteArray(request.readAll()).split(/\r?\n/);
-		request.close();
-		
+		try
+		{
+			var request = new HttpRequest(url + "/get/version", "GET");
+			request.send();
+			var remoteVersionData = CreateStringFromByteArray(request.readAll()).split(/\r?\n/);
+			request.close();
+		}
+		catch (e)
+		{
+			throw new GameUpdaterException("Unable to request version resource.");
+		}
+				
 		var remoteVersionInfo = {};
 		
 		for (var i = 0; i < remoteVersionData.length; i++)
 		{
-			remoteVersionInfo[remoteVersionData[i].substr(0, remoteVersionData[i].indexOf("="))] = remoteVersionData[i].substr(remoteVersionData[i].indexOf("="));
+			remoteVersionInfo[remoteVersionData[i].substr(0, remoteVersionData[i].indexOf("="))] = remoteVersionData[i].substr(remoteVersionData[i].indexOf("=") + 1);
 		}
 		
 		return remoteVersionInfo;
@@ -135,12 +191,17 @@ function GameUpdater(url)
 			return /^http:\/\/(.*?)(\:(\d+))?\/.*$/(uri)[3] || null;
 		}
 		
+		function getPathPart(uri)
+		{
+			return /^http:\/\/(.*?)(\/(.*))$/(uri)[2] || null;
+		}
+		
 		this.send = function()
 		{
 			// build the headers
 			var headers = [];
 			
-			headers.push(this.method.toUpperCase() + " " + this.uri + "HTTP/1.1");
+			headers.push(this.method.toUpperCase() + " " + getPathPart(this.uri) + " HTTP/1.1");
 			headers.push("Host: " + getHostPart(this.uri));
 			headers.push("Accept: text/plain");
 			headers.push("Pragma: no-cache");
@@ -153,33 +214,62 @@ function GameUpdater(url)
 			{
 				throw new HttpRequestException("Unable to open socket.");
 			}
-			this.socket.write(CreateByteArrayFromString(headers.join("\r\n") + "\r\n"));
+			this.socket.write(CreateByteArrayFromString(headers.join("\r\n") + "\r\n\r\n"));
 		}
 		
 		this.readAll = function()
 		{
-			if (!this.socket)
+			if (!this.socket.isConnected())
 			{
 				throw new HttpRequestException("No socket to read from.");
 			}
-			
 			// Read the headers
 			var header_data = "";
 			while (header_data.indexOf("\r\n\r\n") == -1)
 			{
+				if (!this.socket.isConnected()) throw new HttpRequestException("Socket became disconnected during read.");
 				header_data += CreateStringFromByteArray(this.socket.read(this.socket.getPendingReadSize()));
 			}
 			
-			var headers = header_data.split("\r\n");
+			var extra_data = header_data.substr(header_data.indexOf("\r\n\r\n") + 4);
+			var headers = header_data.substr(0, header_data.indexOf("\r\n\r\n")).split("\r\n");
+			
+			// If this was an error (400-599), fail.
+			var status_code = parseInt(headers[0].split(" ")[1]);
+			if (status_code >= 400 && status_code <= 599)
+			{
+				throw new HttpRequestException(headers[0]);
+			}
 			
 			// If the server has sent content-length, we need to use that.
 			var contentLength = -1;
 			for (var i = 0; i < headers.length; i++)
 			{
-				var header_name = headers[i].substr(0, headers[i].indexOf("="));
-				var header_value = headers[i].substr(headers[i].indexOf("="));
-			}			
-			
+				var header_name = headers[i].substr(0, headers[i].indexOf(":"));
+				var header_value = headers[i].substr(headers[i].indexOf(":") + 1);
+				if (header_name.toLowerCase() == "content-length")
+				{
+					contentLength = parseInt(header_value);
+				}
+			}
+			if (contentLength != -1)
+			{
+				return CreateByteArrayFromString(extra_data).concat(this.socket.read(contentLength));
+			}
+			else
+			{
+				var ba = CreateByteArrayFromString(extra_data);
+				while (this.socket.isConnected())
+				{
+					ba = ba.concat(this.socket.read(this.socket.getPendingReadSize()));
+				}
+				return ba;
+			}
+		}
+		
+		this.close = function()
+		{
+			this.socket.close();
 		}
 	}
 }
